71d5f19698d38dc03b1ba532136782d81e4d6ac8
Abdulxamid04 <sayfullayevabdulxamid@gmail.com>
2023-11-01 09:26:50 +0500

    add globalContext

---
+++ src/contex.jsx
@@ -0,0 +1,16 @@
+import { createContext, useContext } from "react";
+
+const AppContext = createContext();
+const AppProvider = (children) => {
+    return(
+        <AppContext.Provider>
+               {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
+
+export {AppContext , AppProvider};
